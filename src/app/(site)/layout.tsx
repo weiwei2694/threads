59b44c3e5bce6243d653787c700feb17bb6de8a6
Arya Priema <99aryaprima@gmail.com>
2023-08-12 04:47:49 +0700

    feat: Topbar

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,36 +1,48 @@
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Manrope } from 'next/font/google'
-import LeftSidebar from '@/components/LeftSidebar'
-import RightSidebar from '@/components/RightSidebar'
+import "../globals.css";
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import Topbar from "@/components/Topbar";
+import { ClerkProvider, currentUser } from "@clerk/nextjs";
+import { fetchUser } from "@/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const manrope = Manrope({ subsets: ['latin'] })
+const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Threads',
-  description: 'create your first thread',
-}
+  title: "Threads",
+  description: "create your first thread",
+};
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const user = await currentUser()
+  if (!user) return null;
+
+  const userInfo = await fetchUser({ id: user.id })
+  if (!userInfo) redirect('/auth/sync-user')
+
   return (
-    <html lang="en">
-      <body className={manrope.className}>
-        <main className="flex flex-row">
-          <LeftSidebar />
+    <ClerkProvider>
+      <html lang="en">
+        <body className={manrope.className}>
+          <Topbar user={userInfo} />
 
-          <section className="main-container">
-            <div className="max-w-4xl w-full">
-              {children}
-            </div>
-          </section>
+          <main className="flex flex-row">
+            <LeftSidebar />
 
-          <RightSidebar />
-        </main>
-      </body>
-    </html>
-  )
+            <section className="main-container">
+              <div className="max-w-4xl w-full">{children}</div>
+            </section>
+
+            <RightSidebar />
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
---
+++ src/components/Topbar.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { User } from "@prisma/client";
+
+interface Props {
+  user: User;
+}
+
+function Topbar({ user }: Props) {
+  const router = useRouter();
+
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        {/* <Image src="/assets/logo.svg" alt="logo" width={28} height={28} /> */}
+        <p className="font-bold text-2xl text-light-1">Threads</p>
+      </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+
+        <div>
+          <Link
+            className="flex flex-row items-center gap-2 cursor-pointer"
+            href="/profile"
+          >
+            <Image
+              src={user.image}
+              alt={user.name}
+              width={40}
+              height={40}
+              className="object-contain rounded-lg"
+            />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Topbar;
