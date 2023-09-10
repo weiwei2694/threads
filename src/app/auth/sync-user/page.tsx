be17fa996bf9ed1aef8c2510f61c8c7df199cb55
Arya Priema <99aryaprima@gmail.com>
2023-09-11 06:07:33 +0700

    fix: fixed bug in '/auth/sync-user'

--- src/app/auth/sync-user/page.tsx
+++ src/app/auth/sync-user/page.tsx
@@ -3,25 +3,21 @@ import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
-  try {
-    const user = await currentUser();
-    if (!user) return null;
+  const user = await currentUser();
 
-    const userInfo = await fetchUser({
-      id: user.id,
-      name: `${user.firstName} ${user.lastName}`,
-      username: user.username?.toLowerCase(),
-      email: user.emailAddresses[0].emailAddress,
-      image: user.imageUrl,
-    });
+  if (!user) return null;
 
-    if (userInfo) redirect("/");
+  const userInfo = await fetchUser({
+    id: user.id,
+    name: `${user.firstName} ${user.lastName && user.lastName}`,
+    username: user.username?.toLowerCase(),
+    email: user.emailAddresses[0].emailAddress,
+    image: user.imageUrl,
+  });
 
-    return null;
-  } catch (error) {
-    console.error("An error occurred:", error);
-    return <div>An error occurred. Please try again later.</div>;
-  }
+  if (userInfo) redirect("/");
+
+  return null;
 };
 
 export default Page;
